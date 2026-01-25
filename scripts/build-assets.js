55e5cb37d9bdfd5d9216a22135e3d8e8405a29b8
Conor <conor.recent017@slmails.com>
2026-01-25 12:46:58 +0000

    Add CNAME to src, update project copy

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -1,5 +1,13 @@
 'use strict';
 
 const renderAssets = require('./render-assets');
+const upath = require('upath');
+const sh = require('shelljs');
 
 renderAssets();
+
+// Copy CNAME file for GitHub Pages
+const sourceCNAME = upath.resolve(upath.dirname(__filename), '../src/CNAME');
+const destCNAME = upath.resolve(upath.dirname(__filename), '../dist/CNAME');
+
+sh.cp(sourceCNAME, destCNAME);
